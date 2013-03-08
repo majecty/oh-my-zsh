b6437919b1335f30951337896d0e1d03429fdb0f
Juhyeong Park <jh@company100.com>
2013-03-08 04:17:31 -0500

    Add useful custom function.
    lt : view recently changed file.
    psgrep : ps | grep
    fname : find -iname

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,9 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+
+
+# From http://mmb.pcb.ub.es/~carlesfe/unix/tricks.txt
+function lt() { ls -ltrsa "$@" | tail ;}
+function psgrep() { ps axuf | grep -v grep | grep "$@" -i --color=auto; }
+function fname() { find . -iname "*$@"; }
